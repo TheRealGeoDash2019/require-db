22c2803fecf0155e8a3689ac7c09f48e4a77d8bf
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-12-12 00:21:23 -0500

    Create yallist.iterator.js

---
+++ remade_modules/yallist.iterator.js
@@ -0,0 +1,7 @@
+module.exports = function (Yallist) {
+  Yallist.prototype[Symbol.iterator] = function* () {
+    for (let walker = this.head; walker; walker = walker.next) {
+      yield walker.value
+    }
+  }
+}
